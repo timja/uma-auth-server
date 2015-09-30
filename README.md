7b5b3c979f298c0e1b770461091b2163ff36edd6
Tim Jacomb <timja@datacom.co.nz>
2015-09-30 16:53:37 +1300

    Tweaks

--- README.md
+++ README.md
@@ -10,3 +10,13 @@ Then run:
 npm install
 ```
 
+Use forever to update after file changes
+```
+npm install -g forever
+```
+
+Then use something like:
+```
+forever --watch --watchDirectory routes/ --watchIgnore *.log bin/www
+```
+
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var fs = require("fs");
+
+var defaultHeaders = { 'Content-Type': 'application/json' };
+
+router.get('/tim', function(req, res, next) {
+    console.log(req.query);
+    console.log(req.params);
+    res.send('hi tim2:');
 });
+
+/* GET users listing. */
+router.get('/:id', function(req, res, next) {
+    console.log(req.query);
+    console.log(req.params);
+    res.send('user: ' + req.params.id);
+});
+
 
 module.exports = router;
